125a1e3e441a971fbfb208735f0c7acf3ed82395
ih8rain <linyuminhao@qq.com>
2017-04-23 18:21:33 +0800

    Initial commit

---
+++ app/src/main/java/fjnu/edu/cse/intent/Second.java
@@ -0,0 +1,35 @@
+package fjnu.edu.cse.intent;
+
+/**
+ * Created by Administrator on 2017/4/23.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+/**
+ * Created by Administrator on 2017/4/11.
+ */
+public class Second extends Activity {
+    WebView webView;
+    String second_url;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+        Bundle bundle = this.getIntent().getExtras();
+        second_url =bundle.getString("URL");
+        init();
+    }
+    private void init(){
+        webView = (WebView) findViewById(R.id.webView);
+        webView.loadUrl(second_url);
+        webView.setWebViewClient(new WebViewClient(){
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+    }
+}
